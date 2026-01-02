14aa5198f1962f3b06a185fe015a2c7f3f4da17a
pks_021127 <parkkyusun021127@gmail.com>
2026-01-03 07:22:22 +0900

    삼각형 출력

--- Main.java
+++ Main.java
@@ -15,8 +15,17 @@ public class Main {
         glfwShowWindow(window);
         
         while(!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
+        	glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT);
+            
+            glBegin(GL_TRIANGLES);
+            	glColor3f(1.0f, 0.0f, 0.0f);
+            	glVertex2f(-0.25f, -0.25f); 
+            	glColor3f(0.0f, 1.0f, 0.0f);
+            	glVertex2f(0.25f, -0.25f);
+            	glColor3f(0.0f, 0.0f, 1.0f);
+            	glVertex2f(0.0f, 0.25f);
+            	glEnd();
             glfwSwapBuffers(window);
         }
         
