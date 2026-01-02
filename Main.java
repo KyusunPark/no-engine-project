a76cd6bd7670a3cdf1e46bc2cf2b04fa1c81203e
pks_021127 <parkkyusun021127@gmail.com>
2026-01-03 06:42:11 +0900

    윈도우 창 생성

---
+++ Main.java
@@ -0,0 +1,26 @@
+import org.lwjgl.opengl.GL;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+public class Main {
+    public static void main(String[] args) {        
+        glfwInit();
+        long window = glfwCreateWindow(800, 600, "LWJGL", 0, 0);
+        glfwSetWindowPos(window, 100, 200);
+        
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        GL.createCapabilities();
+        glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+        glfwShowWindow(window);
+        
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(window);
+        }
+        
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+}
